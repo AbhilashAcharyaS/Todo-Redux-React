783f79b94306ac19fd8b794977feacc2afbc9323
AbhilashAcharyaS <abhilashacharya599@gmail.com>
2025-01-13 15:59:57 +0530

    save redux store to localhost

--- src/store/appStore.js
+++ src/store/appStore.js
@@ -4,7 +4,32 @@ import todoSlice from "./todoSlice"
 const appStore= configureStore({
     reducer:{
         todos:todoSlice
+    },
+    preloadedState:loadFromLocalStorage()
+});
+
+function saveToLocalStorage(state){
+
+    try{
+      const serialState = JSON.stringify(state)
+      localStorage.setItem("reduxStore",serialState)
+    }catch(e){
+      console.warn(e);
     }
-})
+  }
+  
+  function loadFromLocalStorage(){
+
+    try{
+      const serialisedState = localStorage.getItem("reduxStore");
+      if(serialisedState === null) return undefined;
+      return JSON.parse(serialisedState);
+    }catch(e){
+      console.warn(e);
+      return undefined;
+    }
+  }
+
+  appStore.subscribe(()=>saveToLocalStorage(appStore.getState()));
 
 export default appStore;
